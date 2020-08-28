c6637f5e5c85d52270a61e5a736b1aa9fb4b4b94
Douaa yazidi <douaayazidi@gmail.com>
2020-08-28 15:32:45 +0100

    add froms component

---
+++ src/app/froms/froms.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-froms',
+  templateUrl: './froms.component.html',
+  styleUrls: ['./froms.component.css']
+})
+export class FromsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  login(form : NgForm){
+
+  }
+
+}
